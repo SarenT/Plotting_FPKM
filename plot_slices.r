1f785d2ff999110b36825207da3dbbabfebeb40d
melkrewi <marwanelkrewi@gmail.com>
2021-02-26 13:09:15 +0100

    changes

--- plot_slices.r
+++ plot_slices.r
@@ -9,27 +9,21 @@ library(reshape2)
 library(tidyverse) 
 library(ggpubr)
 library("stringr") 
-# Directory? where is the file? (I know ehere it is but you should use relative paths.)
-# https://stackoverflow.com/questions/13672720/r-command-for-setting-working-directory-to-source-file-location-in-rstudio
-# also check "here" package
-# Use comments to explain your code
-RPKM <-fread("~/git/Plotting_FPKM/PLoS_data.txt", sep="auto",head=T)
-RPKM_60u <- RPKM %>% dplyr:: select(V1,starts_with("60"))
+setwd("~/git/Plotting_FPKM/") #setting working directory 
+RPKM <-fread("PLoS_data.txt", sep="auto",head=T) #loading the published data
+RPKM_60u <- RPKM %>% 
+  dplyr::select(V1,starts_with("60"))
 RPKM.df <- setNames(melt(RPKM_60u), c('rows', 'vars', 'values'))
-#RPKM.df <-RPKM_25u %>% gather(vars,values,-X )
 test <- plyr::arrange(RPKM.df, rows)
-emb=c(substr(test$vars,5, 8))
-slice=c(substr(test$vars,10, 13))
-test<- test %>% cbind(emb,slice)
+test <- test %>%
+  tidyr::separate(vars, c("NI","emb","slice","rest"),sep="_",remove = F,extra ="merge")
 plotlist=list()
 names_list=list("lab","pb","Dfd","Scr","Antp","Ubx","abd-A","Abd-B")
-#names_list=list("bcd","Nos","gt","Kr","eve","wg")
-# Do you really need a for loop here? Even then, why is it hard coded for 6?
-for (i in 1:6)
-{
-p <- ggplot(subset(test,rows ==names_list[i]), aes(x = slice, y = rows, fill = values)) + facet_grid(emb ~ ., scales='free_x', space="free_x") + geom_tile() + scale_fill_gradient(low = "#FFFFFF",high = "#012345")+ theme(plot.title = element_text(hjust = 0.5),axis.title.y=element_blank(),axis.ticks.y = element_blank(), axis.text.y = element_blank()) + ggtitle(paste(names_list[i]))
-plotlist[[i]]=p
+# Do you really need a for loop here?  
+for (i in 1:length(names_list)){
+  p <- ggplot(subset(test,rows ==names_list[i]), aes(x = slice, y = rows, fill = values)) + facet_grid(emb ~ ., scales='free_x', space="free_x") + geom_tile() + scale_fill_gradient(low = "#FFFFFF",high = "#012345")+ theme(plot.title = element_text(hjust = 0.5),axis.title.y=element_blank(),axis.ticks.y = element_blank(), axis.text.y = element_blank()) + ggtitle(paste(names_list[i]))
+  plotlist[[i]]=p
 }
-# why not facets?
-main<-grid.arrange(grobs=plotlist,ncol=6)
-ggsave("bigplot.pdf",main,width=40,height=5)
+# why not facets? I think if I use facets for all, then the filling will be relative to the highest expression gene, but I am not sure. 
+main<-grid.arrange(grobs=plotlist,ncol=length(names_list))
+ggsave("hox_genes.pdf",main,width=40,height=5)
